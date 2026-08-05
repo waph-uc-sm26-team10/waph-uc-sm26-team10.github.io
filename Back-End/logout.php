5c17f600a47f552dfd841f0a89099f273bbc3ecb
Mike Wojtkiewicz <mikewojtkiewicz@gmail.com>
2026-08-05 18:12:11 -0400

    Undo last commit

--- Back-End/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-?>
-<p> you are logged out! </p>
-
-<a href="../index.php"> login again </a>
--- Back-End/session_auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() !== PHP_SESSION_ACTIVE) {
-    session_start();
-}
-
-if (empty($_SESSION['userid'])) {
-    header("Location: ../Front-End/loginform.php");
-    exit();
-}
